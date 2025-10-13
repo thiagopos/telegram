2be2748cd275e387cf9948d7cc42b410ea27370c
Thiago Possa <thiagopossa@outlook.com>
2025-10-13 09:04:27 -0300

    ecosystem refatorado

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [{
-    name: "Telegram",
+    name: "telegram",
     script: "./src/index.js",
     autorestart: true,
     watch: false,
     max_memory_restart: '128M',
-    exp_backoff_restart_delay: 5000
+    exp_backoff_restart_delay: 60000
   }]
 }
